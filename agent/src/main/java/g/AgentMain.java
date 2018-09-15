15c93fc05a31d3b894ad4da011092a74b27d20c9
charles.lin <chengjin.lyf@alibaba-inc.com>
2018-09-15 21:46:56 +0800

    modify agent start param

--- agent/src/main/java/g/AgentMain.java
+++ agent/src/main/java/g/AgentMain.java
@@ -10,9 +10,17 @@ import io.vertx.core.Vertx;
 public class AgentMain {
 
     public static void main(String args[]){
-        int agentPort = 4321;
-        String proxyUrl = "127.0.0.1";
-        int proxyPort = 8000;
+        int agentPort;
+        String proxyUrl;
+        int proxyPort;
+        try{
+            agentPort = Integer.parseInt(args[2]);
+            proxyPort = Integer.parseInt(args[1]);
+            proxyUrl = args[0];
+        }catch (Exception e){
+            System.out.println("java -jar proxyIp  proxyPort agentPort");
+            return;
+        }
         Vertx vertx = Vertx.vertx();
         vertx.deployVerticle(new AgentServer(agentPort, proxyUrl, proxyPort));
         vertx.deployVerticle(new StaticsUnit());
